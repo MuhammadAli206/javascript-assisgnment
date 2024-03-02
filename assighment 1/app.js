da03511573e97df838d0b7f7b6faac269ec4d0bc
MuhammadAli206 <154820260+MuhammadAli206@users.noreply.github.com>
2024-03-01 20:13:16 -0800

    code uploaded

---
+++ assighment 1/app.js
@@ -0,0 +1,101 @@
+// chapter1
+
+// 1answer
+
+alert("greetings");
+
+//2 answer
+
+alert("Java script Alert \n Error! please enter a valid password");
+
+//3 answer
+
+alert("welcome to js land... \n Happy coding!");
+
+//4 answer
+
+var message = "welcome to js land";
+
+alert(message);
+
+var message ='happy coding'
+alert(message)
+
+
+//5 answer
+
+console.log("Hello... i can run js through my web browers console");
+
+
+
+//6 answer
+
+    
+
+// Chapter2
+
+
+// answer1
+
+var userName ='Muhammad baloch'
+alert(userName);
+
+// answer2
+
+var myName = "Muhammad Baloch"
+alert(myName);
+
+// answer3
+
+
+
+
+
+
+
+// answer4
+
+
+
+
+// answer5
+
+var jan = "PIZZA \n PIZZ \n PIZ \n PI \n P"
+alert(jan);
+
+// answer6
+
+var aws ="My email address is example@example.com "
+alert(aws);
+
+// answer7
+
+var fws ="I am trying to Learn from the Book A smarter \n way to learn JavaScripts "
+alert(fws);
+
+// answer8
+
+
+Swal.fire({
+    title: "Good job!",
+    text: "You clicked the button!",
+    icon: "success"
+  });
+
+
+
+
+//chapter 3
+
+//answer 1
+
+var name ='i am 17 years old'
+
+// answer 2
+
+var name = 'You have visited this site 14 times'
+
+
+//answwer 3
+
+
---
+++ assighment 2/app.js
@@ -0,0 +1,23 @@
+//chapter 3
+
+//answer 1
+
+var name ='i am 17 years old'
+alert(name)
+
+// answer 2
+
+var namey = 'You have visited this site 14 times'
+alert(namey)
+
+
+//answwer 3 
+
+
+
+//chapter 4
+
+//answer 1
+
+
+
